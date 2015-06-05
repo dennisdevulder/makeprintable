968a6014b226d941eda0cac24d259b24be4c82fe
Dennis de Vulder <dennisdevulder@gmail.com>
2015-06-05 13:54:19 +0200

    Fix specs for new base_uri method

--- spec/endpoints_spec.rb
+++ spec/endpoints_spec.rb
@@ -11,12 +11,12 @@ describe MakePrintable::Client do
   describe '#configure payload' do
     it 'should add an extra variable to the base_uri' do
       client = MakePrintable::Client.new
-      expect(client.configure_payload(variable: true)).to eq "http://api.makeprintable.com/?api_key=123apikey&api_secret=123apisecret&variable=true"
+      expect(client.configure_payload('/', variable: true)).to eq "http://api.makeprintable.com/?api_key=123apikey&api_secret=123apisecret&variable=true"
     end
 
     it 'should have no problems chaining variables' do
       client = MakePrintable::Client.new
-      expect(client.configure_payload(variable1: true, variable2: false)).to eq "http://api.makeprintable.com/?api_key=123apikey&api_secret=123apisecret&variable1=true&variable2=false"
+      expect(client.configure_payload('/', variable1: true, variable2: false)).to eq "http://api.makeprintable.com/?api_key=123apikey&api_secret=123apisecret&variable1=true&variable2=false"
     end
   end
 end
