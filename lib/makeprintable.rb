27ab0d110f6063bf01b07a2af50ac26c2d849515
Dennis de Vulder <dennisdevulder@gmail.com>
2015-06-05 11:36:08 +0200

    Add configuration block + spec

--- lib/makeprintable.rb
+++ lib/makeprintable.rb
@@ -4,4 +4,17 @@ require 'rest-client'
 require 'json'
 
 module MakePrintable
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+
+  class Configuration
+    attr_accessor :api_key, :api_secret
+  end
 end
