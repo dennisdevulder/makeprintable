a6335d8240bf47524d63376b40ff4dee809f0fe3
Dennis de Vulder <dennisdevulder@gmail.com>
2015-06-05 13:51:54 +0200

    Define endpoints and request types

--- lib/makeprintable/client/endpoints.rb
+++ lib/makeprintable/client/endpoints.rb
@@ -1,11 +1,23 @@
 module MakePrintable
   class Client
-    def base_uri
-      "http://api.makeprintable.com/?api_key=#{api_key}&api_secret=#{api_secret}"
+    def base_uri(path='/')
+      "http://api.makeprintable.com#{path}?api_key=#{api_key}&api_secret=#{api_secret}"
     end
 
-    def configure_payload(opts={})
-      base_uri + opts.to_params
+    def configure_payload(path, opts={})
+      base_uri(path) + opts.to_params
+    end
+
+    def get_request(uri)
+      JSON.parse RestClient.get(uri)
+    end
+
+    def post_request(path, opts)
+      JSON.parse RestClient.post(base_uri(path), opts)
+    end
+
+    def delete_request(uri)
+      JSON.parse RestClient.delete(uri)
     end
   end
 end
