913de13bf63dbf82fd4ebf9fea28eef4987264fa
Dennis de Vulder <dennisdevulder@gmail.com>
2015-06-05 13:13:58 +0200

    Add configure payload method + specs

--- lib/makeprintable/client/endpoints.rb
+++ lib/makeprintable/client/endpoints.rb
@@ -3,5 +3,9 @@ module MakePrintable
     def base_uri
       "http://api.makeprintable.com/?api_key=#{api_key}&api_secret=#{api_secret}"
     end
+
+    def configure_payload(opts={})
+      base_uri + opts.to_params
+    end
   end
 end
